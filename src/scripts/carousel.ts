95fbe1d8355fe2f32667e2d1e2ed5296ac14364f
renzoyaques <renzoyaques@gmail.com>
2025-05-05 22:09:07 +0000

    fix: Improve Carousel layout and behavior

--- src/scripts/carousel.ts
+++ src/scripts/carousel.ts
@@ -1,7 +1,7 @@
 import EmblaCarousel, { type EmblaCarouselType, type EmblaOptionsType } from 'embla-carousel'
 
 const initializeCarousels = () => {
-  const OPTIONS: EmblaOptionsType = { skipSnaps: false }
+  const OPTIONS: EmblaOptionsType = { skipSnaps: false, slidesToScroll: 'auto' }
 
   const carousels = document.querySelectorAll('.carousel') as NodeListOf<Element>
 
